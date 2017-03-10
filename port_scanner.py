7e8f80d9124e47c9f7ea903988fe53ce4c95ce3f
Brian Misachi <misachibrian@gmail.com>
2017-03-10 15:12:23 +0300

    Added a new dependency(python fire) and put port scanner in a class(PortScanner)

--- port_scanner.py
+++ port_scanner.py
@@ -1,24 +1,33 @@
-import socket, time
+import socket, time, fire
 
-HOST = ''  # accepts all hosts
-NUM = 0
+class PortScanner:
+    HOST = ''  # accepts all hosts
+    NUM = 0
+
+    def __init__(self, host, num):
+        self.HOST = host
+        self.NUM = num
+
+    def scan_open_ports(self):
+        """Creates new socket each time with a a new
+            port number from a user given range
+        """
+        for PORT in range(NUM):
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s.bind((HOST, PORT)) # This checks if port is open
+                print(str(PORT) + " open") # prints the open sockets
+            except Exception:
+                print('This port is closed: %s' % PORT)
 
 if __name__ == '__main__':
-    t1 = time.time()  # initial time
     HOST = input('Host name: ')
     NUM = int(input('Port range: '))
-
-    """Creates new socket each time with a a new
-        port number from a user given range
-    """
-    for PORT in range(NUM):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((HOST, PORT)) # This checks if port is open
-            print(str(PORT) + " open") # prints the open sockets
-        except Exception:
-            print('This port is closed: %s' % PORT)
+    t1 = time.time()  # initial time
+    
+    port_scan = PortScanner(HOST, NUM)
+    fire.Fire(port_scan.scan_open_ports())
     t2 = time.time()  # finish time
 
     # computes and displays the total time taken to scan all the port within the given range
